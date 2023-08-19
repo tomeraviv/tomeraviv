da7dc9481cb2d5b145d4fcb8180c41bd2abca6c6
tomeraviv <bdps9ixcly6qcjjgiiegc40dikdyy0@bots.bitbucket.org>
2023-08-19 17:07:32 +0300

    Added contact.tsx.

---
+++ tomeraviv_web/src/pages/contact.tsx
@@ -0,0 +1,36 @@
+import {signIn, signOut, useSession} from "next-auth/react";
+import Head from "next/head";
+import {api} from "~/utils/api";
+import {UserCircleIcon} from "@heroicons/react/24/solid";
+import {MyLink} from "~/components/myLink";
+import RiveCatAnimated from "~/components/riveCatAnimated";
+import {GitHubLink} from "~/components/gitHubLink";
+
+export default function Home()
+{
+	return (
+		<>
+			<Head>
+				<title>Tomer Aviv</title>
+				<link rel="icon" href="/favicon.ico"/>
+			</Head>
+
+			<GitHubLink repo_url="https://github.com/tomeraviv/tomeraviv"/>
+
+			<main className="flex min-h-screen flex-col items-center justify-center dark:bg-gray-950">
+				<div className="container flex flex-grow flex-col items-center justify-center gap-6 px-4 sm:gap-12">
+					<h1 className="text-lg font-extrabold tracking-tight text-black dark:text-white sm:text-3xl">
+						<UserCircleIcon className="block text-lg text-black dark:text-white"/>
+						Tomer Aviv
+					</h1>
+
+					<div className="grid w-60 grid-cols-1 gap-2 sm:w-80 md:gap-2">
+						<RiveCatAnimated/>
+						<MyLink text="Back" href="/"/>
+					</div>
+				</div>
+				<div className="flex-grow w-full h-full"/>
+			</main>
+		</>
+	);
+}
