0f16c45044a9e7e337f96aa5e236d3ceb64cb65e
tomeraviv <bdps9ixcly6qcjjgiiegc40dikdyy0@bots.bitbucket.org>
2023-09-03 20:15:15 +0300

    Added dark mode toggle stub.

--- tomeraviv_web/src/pages/index.tsx
+++ tomeraviv_web/src/pages/index.tsx
@@ -1,10 +1,9 @@
-import {signIn, signOut, useSession} from "next-auth/react";
 import Head from "next/head";
-import {api} from "~/utils/api";
 import {UserCircleIcon} from "@heroicons/react/24/solid";
 import {MyLink} from "~/components/myLink";
 import RiveCatAnimated from "~/components/riveCatAnimated";
 import {GitHubLink} from "~/components/gitHubLink";
+import {ToggleDarkMode} from "~/components/toggleDarkMode";
 
 export default function Home()
 {
@@ -15,7 +14,11 @@ export default function Home()
 				<link rel="icon" href="/favicon.ico"/>
 			</Head>
 
-			<GitHubLink repo_url="https://github.com/tomeraviv/tomeraviv"/>
+			<div className="absolute top-0 m-3 gap-2 right-0 flex items-center">
+				<ToggleDarkMode/>
+				<GitHubLink repo_url="https://github.com/tomeraviv/tomeraviv"/>
+			</div>
+
 
 			<main className="flex min-h-screen flex-col items-center justify-center dark:bg-gray-950">
 				<div className="container flex flex-grow flex-col items-center justify-center gap-6 px-4 sm:gap-12">
@@ -26,13 +29,12 @@ export default function Home()
 
 					<div className="grid w-60 grid-cols-1 gap-2 sm:w-80 md:gap-2">
 						<RiveCatAnimated/>
-						<MyLink text="Résumé" href="/cv"/>
 						<MyLink text="LinkedIn" href="https://www.linkedin.com/in/tomer-aviv-link/" is_external={true}/>
-						<MyLink text="Oniverkita.co.il" href="https://www.oniverkita.co.il/" is_external={true}/>
+						<MyLink text="Oniverkita" href="https://www.oniverkita.co.il/" is_external={true}/>
 						<MyLink text="Send Me A Message" href="/contact"/>
 					</div>
 				</div>
-				<div className="flex-grow w-full h-full"/>
+				<div className="h-full w-full flex-grow"/>
 			</main>
 		</>
 	);
