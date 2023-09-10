df6f476ca738188599b2d492be14f15e81d830fb
tomeraviv <bdps9ixcly6qcjjgiiegc40dikdyy0@bots.bitbucket.org>
2023-09-10 13:23:14 +0300

    Changed SVG attrib. naming to JSX. Using resolvedTheme in case user has no preference cookie. (We have 'enableSystem=true' on ThemeProvider) Updated package.json.

--- tomeraviv_web/src/components/toggleDarkMode.tsx
+++ tomeraviv_web/src/components/toggleDarkMode.tsx
@@ -1,12 +1,11 @@
-import {useState} from "react";
 import {useTheme} from 'next-themes'
 
 function MoonIcon()
 {
 	return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5">
-		<path fill-rule="evenodd"
+		<path fillRule="evenodd"
 			  d="M7.455 2.004a.75.75 0 01.26.77 7 7 0 009.958 7.967.75.75 0 011.067.853A8.5 8.5 0 116.647 1.921a.75.75 0 01.808.083z"
-			  clip-rule="evenodd"/>
+			  clipRule="evenodd"/>
 	</svg>;
 }
 
@@ -21,9 +20,9 @@ function SunIcon()
 
 export function ToggleDarkMode()
 {
-	const {theme, setTheme} = useTheme();
+	const {setTheme, resolvedTheme} = useTheme();
 
-	return <button title="Toggle dark mode" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')} className="btn">
-		{theme == 'light' ? <MoonIcon/> : <SunIcon/>}
+	return <button title="Toggle dark mode" onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')} className="btn">
+		{resolvedTheme !== 'dark' ? <MoonIcon/> : <SunIcon/>}
 	</button>
 }
